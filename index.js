48c46b9a0878056cc4b6bf28f67f2e325409ba8a
Krishil Rohit Sheth <97043368+krishilsheth404@users.noreply.github.com>
2024-03-17 21:53:47 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+// app using yahoo.com as a search engine
+const express = require('express'); // Include ExpressJS
+const app = express(); // Create an ExpressJS app
+const bodyParser = require('body-parser'); // Middleware 
+const axios = require('axios');
+const path = require('path');
+const request = require('request');
+const { cpSync } = require('fs');
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname));
+
+// app.set('views', './');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+// var newItem;
+// Route to Login Page
+app.get('/', (req, res) => {
+    // console.log((req.query['q']));
+    res.sendFile(__dirname + '/index.html');
+});
+app.post('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+
+app.post('/sendData', (req, res) => {
+    console.log(req.body)
+    axios.post('http://localhost:3000/customer',req.body)
+    .then(() => {
+        console.log("data sent")
+      })
+      .catch((e) => {
+        console.log("Could not send");
+        console.log(e)
+      });
+});
+
+
+
+
+
+const port = process.env.PORT || 1000 // Port we will listen on
+
+// Function to listen on the port
+app.listen(port, () => console.log(`This app is listening on port ${port}`));
